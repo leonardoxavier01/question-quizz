deca35e40a6d91ce6c018caf08ad896e334fe66b
Leonardo -Software Developer <leonardoxavier092@gmail.com>
2022-08-09 20:06:00 -0400

    test github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello world 2</h1>
+      <h1>Hello world</h1>
     </div>
   );
 }
